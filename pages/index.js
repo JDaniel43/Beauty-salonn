2e842128efcd1c422920c2dc3109a87a8ecf47e4
jDaniel43 <jd.rdgzmata@gmail.com>
2024-06-08 10:43:55 -0700

    commit

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useState } from "react"
 import { ProductList } from "@/components/ProductList"
 import { Header } from "@/components/Header"
 import Footer from "@/components/Footer";
-export default function loveMe(){
+export default function LoveMe(){
     const [allProducts, setAllProducts] = useState([]);
 	const [total, setTotal] = useState(0);
 	const [countProducts, setCountProducts] = useState(0);
